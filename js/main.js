e94479acefd2fafa3cdeb58794738d929aaf4fe4
vanbasx <ivan.bas.vb@gmail.com>
2023-07-02 16:20:03 +0200

    add automation section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function () {
-
+    var mixer = mixitup('.automation__wrapper', {
+        load: {
+            filter: '.with'
+        },
+        animation: {
+            duration: 700
+        }
+    });
 })
